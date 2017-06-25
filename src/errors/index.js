60cbd62de3777a75affa1e79548e89ef38908afc
Philip Senger <philip.senger@iag.com.au>
2017-06-25 19:47:07 +1000

    mod errors index
    * adde more doco to the error index file

--- src/errors/index.js
+++ src/errors/index.js
@@ -1,6 +1,11 @@
 let errors = require('restify').errors;
 let InvalidArgumentError = errors.InvalidArgumentError;
 
+/**
+ * Errors, as more and more errors are created, it make sense to add them here.
+ *
+ * @type {{InvalidArgumentError: errors.InvalidArgumentError}}
+ */
 module.exports = {
   InvalidArgumentError: new InvalidArgumentError('Invalid Argument')
 };
